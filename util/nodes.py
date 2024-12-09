334604f3af21401e0388e30cd2279e6e87141de1
Natalie DellaMaria <nataliedellamaria@MacBook-Pro-44.local>
2024-12-08 19:20:34 -0500

    add saving tool

--- util/nodes.py
+++ util/nodes.py
@@ -2,9 +2,15 @@ from langchain_openai import ChatOpenAI
 from util.tools import tools
 from langgraph.prebuilt import ToolNode
 
-system_prompt = "You are a helpful assistant"
+system_prompt = (
+    "You are a helpful assistant who is helping a user store their reading progress of any content."
+    + "It does not have to be a book. It can be an article, blog post, or any other form of content."
+    + "Please prompt the user for what they are currently reading. Then research for the author and summary."
+    + "Finally, ask the user for their progress and store the information."
+)
 
-llm = ChatOpenAI(model = "gpt-4o").bind_tools(tools)
+llm = ChatOpenAI(model="gpt-4o").bind_tools(tools)
+
 
 def should_continue(state):
     last_message = state["messages"][-1]
@@ -12,10 +18,12 @@ def should_continue(state):
         return "end"
     return "continue"
 
+
 def call_model(state):
     messages = state["messages"]
     messages = [{"role": "system", "content": system_prompt}] + messages
     response = llm.invoke(messages)
     return {"messages": [response]}
+
 
 tool_node = ToolNode(tools)
--- util/tools.py
+++ util/tools.py
@@ -1,4 +1,17 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
+import json
+import models.content as Content
 
-tool = TavilySearchResults(max_results=2)
-tools = [tool]
+search_tool = TavilySearchResults(max_results=2)
+
+
+def save_content(content: Content):
+    """Save the content object to a JSON file.
+        
+        Args:
+            content: The content information saved in a Contact object.
+    """
+    with open("content.json", "w") as f:
+        json.dump(content.model_dump(),f,indent=4,default=str)
+
+tools = [search_tool, save_content]
